2d8fe438dbb59e7ed6adc66d4b71a117be4cf5d5
Jakub Stejskal <jakub.stejskal@czechgeeks.cz>
2013-03-31 15:17:21 +0200

    Projectile collision detection added. Game ending events handling added.

--- js/entities/projectile.js
+++ js/entities/projectile.js
@@ -15,10 +15,26 @@ Projectile.prototype.update = function(game) {
   if (this.isOutOfBounds()) {
     this.destroy();
   }
+
   if (this.hostile) {
+    targets = game.entities.players;
     this.moveDown();
   }
   else {
+    targets = game.entities.enemies;
     this.moveUp();
+  }
+
+  this.handleCollisionsWith(targets);
+};
+
+Projectile.prototype.handleCollisionsWith = function(targets) {
+  for (var i = targets.length - 1; i >= 0; i--) {
+    var target = targets[i];
+    if (this.right() > target.left() && this.left() < target.right() &&
+      this.top() < target.bottom() && this.bottom() > target.top()) {
+      this.destroy();
+      target.destroy();
+    }
   }
 };
--- js/game.js
+++ js/game.js
@@ -1,93 +1,115 @@
-var Game = function(view) {
-  this.view = view || new View();
-  this.view.game = this;
-  this.entities = {
-    players: [],
-    enemies: [],
-    projectiles: []
-  };
-
-  this.tempo = Constants.fps / 2;
-  this.time = 0;
-  this.beat = 0;
-};
-
-Game.prototype.start = function() {
-  this.view.display();
-  this.entities.players.push(new Player());
-  this.entities.enemies.push(new Enemy(1,1));
-  this.entities.enemies.push(new Enemy(3,1));
-  this._onEachFrame(Game.prototype.run);
-};
-
-Game.prototype._onEachFrame = (function() {
-  var requestAnimationFrame = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
-
-  if (requestAnimationFrame) {
-    return function(callback) {
-      var caller = this;
-      var _cb = function() { callback.call(caller); requestAnimationFrame(_cb); };
-      _cb();
+  var Game = function(view) {
+    this.view = view || new View();
+    this.view.game = this;
+    this.entities = {
+      players: [],
+      enemies: [],
+      projectiles: []
     };
-  } else {
-    return function(callback) {
-      setInterval(callback, 1000 / Constants.fps);
-    };
-  }
-})();
 
-Game.prototype.run = (function() {
-  var loops = 0,
-  skipTicks = 1000 / Constants.fps,
-  maxFrameSkip = 10,
-  nextGameTick = (new Date()).getTime(),
-  lastGameTick;
-
-  return function() {
-    loops = 0;
-    while ((new Date()).getTime() > nextGameTick) {
-      this.update();
-      nextGameTick += skipTicks;
-      loops++;
-    }
-
-    if (loops) this.view.draw();
-  };
-})();
-
-Game.prototype.update = function() {
-  this.time++;
-  if (this.time == this.tempo) {
+    this.tempo = Constants.fps / 2;
     this.time = 0;
-    this.beat = (this.beat % 4) + 1;
+    this.beat = 0;
+    this.paused = false;
+  };
 
-    var shooter = this.entities.enemies[Math.floor(Math.random() * this.entities.enemies.length)];
-    shooter.shouldShoot = true;
-    this.forEachOf(this.entities.enemies, "update", this);
-  }
-  this.forEachOf(this.entities.players,"update", this);
-  this.forEachOf(this.entities.projectiles,"update", this);
-};
+  Game.prototype.start = function() {
+    this.view.display();
+    this.entities.players.push(new Player());
+    this.entities.enemies.push(new Enemy(1,1));
+    this.entities.enemies.push(new Enemy(3,1));
+    this._onEachFrame(Game.prototype.run);
+  };
 
-Game.prototype.forEachOf = function (list, fn, arg) {
-  for (var i = list.length - 1; i >= 0; i--) {
-    if (list[i].destroyed) {
-      list.splice(i, 1);
+  Game.prototype._onEachFrame = (function() {
+    var requestAnimationFrame = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
+
+    if (requestAnimationFrame) {
+      return function(callback) {
+        var caller = this;
+        var _cb = function() { callback.call(caller); requestAnimationFrame(_cb); };
+        _cb();
+      };
+    } else {
+      return function(callback) {
+        setInterval(callback, 1000 / Constants.fps);
+      };
     }
-    else {
-      list[i][fn](arg);
+  })();
+
+  Game.prototype.run = (function() {
+    var loops = 0,
+    skipTicks = 1000 / Constants.fps,
+    maxFrameSkip = 10,
+    nextGameTick = (new Date()).getTime(),
+    lastGameTick;
+
+    return function() {
+      loops = 0;
+      while ((new Date()).getTime() > nextGameTick) {
+        this.update();
+        nextGameTick += skipTicks;
+        loops++;
+      }
+
+      if (loops) this.view.draw();
+    };
+  })();
+
+  Game.prototype.update = function() {
+    if (!this.paused) {
+      this.checkGameEvents();
+
+      this.time++;
+      if (this.time == this.tempo) {
+        this.time = 0;
+        this.beat = (this.beat % 4) + 1;
+
+        this.enemyShoot();
+        this.forEachOf(this.entities.enemies, "update", this);
+      }
+      this.forEachOf(this.entities.players,"update", this);
+      this.forEachOf(this.entities.projectiles,"update", this);
     }
-  }
-};
+  };
 
-Game.prototype.forEachEntity = function (fn, arg) {
-  this.forEachOf(this.entities.players, fn, arg);
-  this.forEachOf(this.entities.enemies, fn, arg);
-  this.forEachOf(this.entities.projectiles, fn, arg);
-};
+  Game.prototype.enemyShoot = function() {
+    if (this.beat % 2 == 1) {
+      var shooter = this.entities.enemies[Math.floor(Math.random() * this.entities.enemies.length)];
+      shooter.shouldShoot = true;
+    }
+  };
 
-Game.prototype.onShot = function(projectile) {
-  if (projectile) {
-    this.entities.projectiles.push(projectile);
-  }
-};
+  Game.prototype.forEachOf = function (list, fn, arg) {
+    for (var i = list.length - 1; i >= 0; i--) {
+      if (list[i].destroyed) {
+        list.splice(i, 1);
+      }
+      else {
+        list[i][fn](arg);
+      }
+    }
+  };
+
+  Game.prototype.forEachEntity = function (fn, arg) {
+    this.forEachOf(this.entities.players, fn, arg);
+    this.forEachOf(this.entities.enemies, fn, arg);
+    this.forEachOf(this.entities.projectiles, fn, arg);
+  };
+
+  Game.prototype.onShot = function(projectile) {
+    if (projectile) {
+      this.entities.projectiles.push(projectile);
+    }
+  };
+
+  Game.prototype.checkGameEvents = function() {
+    if (this.entities.enemies.length === 0) {
+      this.paused = true;
+      console.log("Game won.");
+    }
+    if (this.entities.players.length === 0){
+      this.paused = true;
+      console.log("Game lost.");
+    }
+  };
