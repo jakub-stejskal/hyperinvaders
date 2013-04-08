b03098d85918b259052b71b96d49c5054c7c1dca
Jakub Stejskal <jakub.stejskal@czechgeeks.cz>
2013-04-09 00:11:33 +0200

    Changed from hashchange plugin to native jQuery event

--- js/navigation.js
+++ js/navigation.js
@@ -1,33 +1,33 @@
 $( document ).ready(function() {
 
-	$("a[href^=#]").hover(
-	function () {
-		$("#cube").addClass("hover-"+ $(this).attr('class'));
-	},
-	function () {
-		$("#cube").removeClass("hover-"+ $(this).attr('class'));
-	}
-	);
+  $("a[href^=#]").hover(
+  function () {
+    $("#cube").addClass("hover-"+ $(this).attr('class'));
+  },
+  function () {
+    $("#cube").removeClass("hover-"+ $(this).attr('class'));
+  }
+  );
 
-	$(window).hashchange(function(e) {
-		navigate();
-	});
+  $(window).on('hashchange', function() {
+    navigate();
+  });
 
-	function navigate() {
-		target = window.location.hash.substr(1);
-		if (target === "play") {
-			$("#cube").attr("class","rotate-game");
-			$("body").addClass("playing");
-		}
-		else if (target === "pause") {
-			$("#cube").attr("class","rotate-game");
-			$("body").removeClass("playing");
-		}
-		else {
-			$("body").removeClass("playing");
-			$("#cube").attr("class","rotate-" + target);
-		}
-	}
+  function navigate() {
+    target = window.location.hash.substr(1);
+    if (target === "play") {
+      $("#cube").attr("class","rotate-game");
+      $("body").addClass("playing");
+    }
+    else if (target === "pause") {
+      $("#cube").attr("class","rotate-game");
+      $("body").removeClass("playing");
+    }
+    else {
+      $("body").removeClass("playing");
+      $("#cube").attr("class","rotate-" + target);
+    }
+  }
 
-	navigate();
+  navigate();
 });
